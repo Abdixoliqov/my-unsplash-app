616f16c6ffbc5ea3940585d55002accd0bfdea4d
Abdixoliqov <abdixoliqvhamidullo@gmail.com>
2024-10-09 12:53:38 +0500

    image info updated

--- src/pages/ImageInfo.jsx
+++ src/pages/ImageInfo.jsx
@@ -1,10 +1,18 @@
 import { useParams } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
 
 
 function ImageInfo() {
   const {id} = useParams()
+  const {data, isPending, error} =  useFetch(
+    `https://api.unsplash.com/photos/${id}?client_id=${import.meta.env.VITE_ACCESS_KEY}`
+  )
+  console.log(data, 'data11112');
+  
   return ( 
-    <div>imageinfo-{id}</div>
+    <div className="align-elements">
+      {/* <img src={data} alt="" /> */}
+    </div>
    );
 }
 
