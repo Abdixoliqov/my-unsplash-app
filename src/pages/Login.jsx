de7f83f23a9cec3ada8921f092f4e5b98cf2b6d4
Abdixoliqov <abdixoliqvhamidullo@gmail.com>
2024-10-24 13:38:31 +0500

    GoogleAuthProvider in Login Page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,12 +4,20 @@ import { Form, Link } from "react-router-dom";
 // components
 import { FormInput } from "../components";
 
+// useRegister hooks
+import { useRegister } from "../hooks/useRegister";
+
 function Login() {
+  const { registerWithGoogle } = useRegister();
   return (
     <div className="flex min-h-screen w-full">
       <div className="hidden w-[40%] bg-[url('https://picsum.photos/900/1200')] bg-cover bg-center md:block"></div>
+      <div className="fixed bottom-0 left-0 right-0 top-0 w-full bg-black opacity-65 md:hidden"></div>
       <div className="flex w-full items-center justify-center bg-[url('https://picsum.photos/900/1200')] bg-cover bg-center md:w-[60%] md:bg-none">
-        <Form method="post" className="w-full max-w-96 px-5 md:px-0">
+        <Form
+          method="post"
+          className="relative z-50 w-full max-w-96 px-5 md:px-0"
+        >
           <h1 className="mb-5 text-center text-4xl font-semibold text-white md:text-black">
             Login
           </h1>
@@ -31,13 +39,14 @@ function Login() {
               Login
             </button>
             <button
+              onClick={registerWithGoogle}
               type="button"
               className="btn btn-secondary btn-sm grow md:btn-md"
             >
               Google
             </button>
           </div>
-          <div className="flex justify-between">
+          <div className="flex flex-col justify-between text-center md:flex-row">
             <p className="text-white md:text-black">Forget password?</p>
             <Link
               to={"/register"}
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -12,8 +12,10 @@ function Register() {
   return (
     <div className="flex min-h-screen w-full">
       <div className="hidden w-[40%] bg-[url('https://picsum.photos/900/1200')] bg-cover bg-center md:block"></div>
+      <div className="fixed top-0 bottom-0 right-0 left-0 w-full bg-black bg-opacity-65 md:hidden"></div>
+      
       <div className="flex w-full items-center justify-center bg-[url('https://picsum.photos/900/1200')] bg-cover bg-center md:w-[60%] md:bg-none">
-        <Form method="post" className="w-full max-w-96 px-5 md:px-0">
+        <Form method="post" className="w-full max-w-96 px-5 md:px-0 relative z-50">
           <h1 className="mb-5 text-center text-4xl font-semibold text-white md:text-black">
             Register
           </h1>
